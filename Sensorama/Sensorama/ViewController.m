1a9a6ff56ff5d0a5b8a00505e18fdeaa12e873f0
Wojciech A. Koszek <wojciech@koszek.com>
2016-02-08 23:55:11 -0800

    Add the accelelerometer polling.

--- Sensorama/Sensorama/ViewController.m
+++ Sensorama/Sensorama/ViewController.m
@@ -7,16 +7,31 @@
 
 #import "ViewController.h"
 #import "Crashlytics/Crashlytics.h"
+#import "CoreMotion/CoreMotion.h"
+
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *forceCrashButton;
 
+@property (strong, nonatomic) CMMotionManager *motionManager;
 @end
 
 @implementation ViewController
 
+- (void)sensoramaStart {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        self.motionManager = [CMMotionManager new];
+    });
+
+    NSLog(@"acc act:%d avail:%d",
+          self.motionManager.accelerometerActive,
+          self.motionManager.accelerometerAvailable);
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self sensoramaStart];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
