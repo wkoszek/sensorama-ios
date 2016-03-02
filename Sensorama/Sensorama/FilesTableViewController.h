a1a6624b42f0e3f740c95a454fc66ef9b8a183fe
Wojciech A. Koszek <wojciech@koszek.com>
2016-03-01 21:51:38 -0800

    Add properly set navbar item title.

---
+++ Sensorama/Sensorama/FilesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FilesTableViewController.h
+//  Sensorama
+//
+//  Created by Wojciech Koszek (home) on 3/1/16.
+//  Copyright © 2016 Wojciech Adam Koszek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilesTableViewController : UITableViewController
+
+@end
---
+++ Sensorama/Sensorama/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  Sensorama
+//
+//  Created by Wojciech Koszek (home) on 3/1/16.
+//  Copyright © 2016 Wojciech Adam Koszek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
