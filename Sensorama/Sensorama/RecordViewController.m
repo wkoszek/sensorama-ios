2046791a02367b65d00af5fa4cfde3b0c7ddb998
Wojciech A. Koszek <wojciech@koszek.com>
2016-03-01 21:30:15 -0800

    Introduce animated start/stop.

--- Sensorama/Sensorama/RecordViewController.m
+++ Sensorama/Sensorama/RecordViewController.m
@@ -4,7 +4,8 @@
 //
 //  Created by Wojciech Koszek (home) on 3/1/16.
 //
-//
+// Corner radius animation:
+// http://stackoverflow.com/questions/5948167/uiview-animatewithduration-doesnt-animate-cornerradius-variation
 
 #import "RecordViewController.h"
 #import "SRUtils.h"
@@ -12,44 +13,69 @@
 @interface RecordViewController ()
 @property (weak, nonatomic) IBOutlet UIView *recordView;
 @property (strong, nonatomic) IBOutlet UITapGestureRecognizer *gestureStartStop;
-
+@property (nonatomic) BOOL isRecording;
+@property (nonatomic) CGFloat savedCornerRadius;
 @end
 
 @implementation RecordViewController
 
-- (void)makeRecordCircle {
-    CGFloat w = self.recordView.frame.size.width;
-    CGFloat h = self.recordView.frame.size.height;
-    NSAssert(w == h, @"recordView must be rectangle");
-    self.recordView.alpha = 1;
-    self.recordView.layer.cornerRadius = h / 2;
-    self.recordView.backgroundColor = [SRUtils mainColor];
+- (void)viewDidAppear:(BOOL)animated
+{
+    NSLog(@"%s", __func__);
+    [super viewDidAppear:animated];
+    [self.tabBarController.navigationItem setTitle:@"Record"];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setIsRecording:false];
+}
 
-    [self makeRecordCircle];
+- (void)setIsRecording:(BOOL)isRecording
+{
+    NSLog(@"%s", __func__);
+
+    [self makeStartStopTransition:isRecording];
+    _isRecording = isRecording;
+}
+
+- (void)makeStartStopTransition:(BOOL)needSquare {
+    NSLog(@"%s", __func__);
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        [self makeInitialCircle];
+    });
+
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"cornerRadius"];
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    animation.fromValue = [NSNumber numberWithFloat:needSquare ? self.savedCornerRadius : 0];
+    animation.toValue = [NSNumber numberWithFloat:needSquare ? 0.0f : self.savedCornerRadius];
+    animation.duration = 0.1;
+    [self.recordView.layer addAnimation:animation forKey:@"cornerRadius"];
+    [self.recordView.layer setCornerRadius:needSquare ? 0 : self.savedCornerRadius];
+}
+
+- (void)makeInitialCircle {
+    NSLog(@"%s", __func__);
+    NSAssert(self.recordView.frame.size.width == self.recordView.frame.size.height,
+             @"recordView must be rectangle");
+    self.recordView.alpha = 1;
+    self.recordView.layer.cornerRadius = self.recordView.frame.size.height / 2;
+    self.recordView.backgroundColor = [SRUtils mainColor];
+
+    self.savedCornerRadius = self.recordView.layer.cornerRadius;
+}
+
+- (IBAction)doStartStop:(UITapGestureRecognizer *)sender {
+    NSLog(@"%s", __func__);
+
+    [self setIsRecording:!self.isRecording];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-- (IBAction)doStartStop:(UITapGestureRecognizer *)sender {
-    [UIView animateWithDuration:10.0
-                          delay:0
-                        options: UIViewAnimationOptionCurveLinear
-                 animations:^{
-                     self.recordView.layer.cornerRadius = 0;
-                     //NSLog(@"animate");
-                 }
-                 completion:^(BOOL finished){
-                     NSLog(@"Done!");
-                 }];
-}
-
 
 /*
 #pragma mark - Navigation
