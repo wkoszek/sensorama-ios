14714723bf8928fd479f8c2e377289464ca99ac7
Wojciech A. Koszek <wojciech@koszek.com>
2016-03-31 23:58:21 -0700

    Add the profile icon to the project

--- scripts/icons_get.sh
+++ scripts/icons_get.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-./iconfinder iconFilesNew_unmodified 370083
+#./iconfinder iconProfile https://www.iconfinder.com/icons/211794/download/png/512
+#./iconfinder iconProfileActive https://www.iconfinder.com/icons/211795/download/png/512
+
+./iconfinder iconProfile https://www.iconfinder.com/icons/657909/download/png/512
+./iconfinder iconProfileActive https://www.iconfinder.com/icons/657911/download/png/512
+
+#./iconfinder iconFilesNew_unmodified 370083
