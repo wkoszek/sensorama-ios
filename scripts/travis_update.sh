0d2d187bb64fec1f0e9ddc18a978141258cb6a89
Wojciech Adam Koszek <wojciech@koszek.com>
2016-05-21 23:07:12 -0700

    Print the correct usage of travis_update

--- scripts/travis_update.sh
+++ scripts/travis_update.sh
@@ -5,4 +5,10 @@
 # I have variables for Fastlane/Crashlytics/Slack and updates
 # the Travis CI variables.
 
+if [ $# -ne 1 ]; then
+	echo "usage"
+	echo "  travis_update.sh file"
+	exit
+fi
+
 grep export $1 | sed 's/=/ /' | awk '{ printf("travis env set %s %s\n", $2, $3); }'
