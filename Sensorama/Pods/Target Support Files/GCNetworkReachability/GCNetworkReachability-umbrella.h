628be9210b055d03dcbcfd50cd6a00e47d1b7f08
Wojciech Adam Koszek <wojciech@koszek.com>
2016-05-31 23:57:55 -0700

    Add network reachability pod.

---
+++ Sensorama/Pods/Target Support Files/GCNetworkReachability/GCNetworkReachability-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "GCNetworkReachability.h"
+
+FOUNDATION_EXPORT double GCNetworkReachabilityVersionNumber;
+FOUNDATION_EXPORT const unsigned char GCNetworkReachabilityVersionString[];
+
