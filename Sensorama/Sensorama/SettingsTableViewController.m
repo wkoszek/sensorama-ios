a4c1b6e1c8a9e097cd9f1c67f12bcce9c605cf3d
Wojciech Adam Koszek <wojciech@koszek.com>
2016-06-05 02:00:42 -0700

    Remove a junk from comment (not relevant)

--- Sensorama/Sensorama/SettingsTableViewController.m
+++ Sensorama/Sensorama/SettingsTableViewController.m
@@ -59,12 +59,6 @@
     [super viewDidLoad];
 
     [SRUsageStats eventAppSettings];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 -(NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
