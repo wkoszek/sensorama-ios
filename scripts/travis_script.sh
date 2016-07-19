0ba889ba77dec20f3f6cddf154e6c0d86793781f
Wojciech Adam Koszek <wojciech@koszek.com>
2016-07-18 22:26:48 -0700

    Move the Travis script to scripts/
    Apparently this should help me with folding.

---
+++ scripts/travis_script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This is meant to be run from top-level dir. of sensorama-ios
+
+cd Sensorama/ && xcrun xcodebuild -list -workspace Sensorama.xcworkspace && cd ..
+./scripts/travis_fold start bootstrapping
+./build.sh bootstrap
+./scripts/travis_fold end bootstrapping
+cd Sensorama && xcrun xcodebuild -list -workspace ./Sensorama.xcworkspace
+cd Sensorama && scan --workspace Sensorama.xcworkspace --scheme SensoramaTests
+./build.sh
