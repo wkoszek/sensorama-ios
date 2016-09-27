e8ee6cde9b7740a0c10b6c41c520dc6bf687f45e
Wojciech Adam Koszek <wojciech@koszek.com>
2016-09-27 00:49:09 -0700

    Add some UI tests

--- Sensorama/SensoramaUITests/SensoramaUITests.m
+++ Sensorama/SensoramaUITests/SensoramaUITests.m
@@ -31,9 +31,67 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void) testTabRecord {
+    XCUIElement *element = [[[[[[[[[[[XCUIApplication alloc] init] childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element;
+    [element tap];
+}
+
+- (void) testTabFiles {
+    XCUIElementQuery *tabBarsQuery = [[XCUIApplication alloc] init].tabBars;
+    [tabBarsQuery.buttons[@"Files"] tap];
+    [tabBarsQuery.buttons[@"Record"] tap];
+}
+
+- (void) testTabSettings {
+    XCUIElementQuery *tabBarsQuery = [[XCUIApplication alloc] init].tabBars;
+    [tabBarsQuery.buttons[@"Settings"] tap];
+    [tabBarsQuery.buttons[@"Record"] tap];
+}
+
+- (void) testTabSlideBackAndForth {
+    XCUIElementQuery *tabBarsQuery = [[XCUIApplication alloc] init].tabBars;
+    XCUIElement *recordButton = tabBarsQuery.buttons[@"Record"];
+    XCUIElement *filesButton = tabBarsQuery.buttons[@"Files"];
+    XCUIElement *settingsButton = tabBarsQuery.buttons[@"Settings"];
+
+    [recordButton tap];
+    [filesButton tap];
+    [settingsButton tap];
+
+    [filesButton tap];
+    [recordButton tap];
+
+    [filesButton tap];
+    [settingsButton tap];
+
+    [recordButton tap];
+}
+
+- (void) helperRemoveFirstFile {
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tabBars.buttons[@"Files"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    [[tablesQuery.cells elementAtIndex:0] tap];
+    [tablesQuery.staticTexts[@"Delete"] tap];
+    [app.alerts[@"Will delete file"].buttons[@"Yes"] tap];
+}
+
+- (void) testRemoveFirstFile {
+    [self helperRemoveFirstFile];
+}
+
+- (void) testRemoveAllFiles {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tabBars.buttons[@"Files"] tap];
+
+    XCUIElementQuery *tablesQuery = app.tables;
+    NSInteger count = [tablesQuery.staticTexts count];
+
+    for (int i = 0; i < count; i++) {
+        [self helperRemoveFirstFile];
+    }
 }
 
 @end
